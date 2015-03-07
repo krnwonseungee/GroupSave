a467a512bae9b2e88dcaffe6901768a571591f23
Samuel Fahlberg <sam.fahlberg@gmail.com>
2015-03-07 15:38:07 -0800

    add photos

---
+++ hack.trips.php
@@ -0,0 +1,36 @@
+<div class='container-fluid row'>
+	<div class='col-xs-12'>
+		<div id='groupSave' class='dTab'>
+
+			<div class='groupSaveContent' rel='10'>
+				<div class='header'>Hawaii Trip 4/1-4/15</div>
+					<div><img src='/images/hawaii.jpg' class='img-thumbnail' style='width: 100%'></div>
+					<div><img src='/images/profile1.jpg' class='img-circle' style='width: 40px; float:left;'></div>
+				<div style='float: left;'>Phitha<br>xx minutes ago</div>
+				<div style='clear: both;'></div>
+				<div>Detail</div>
+				<div>Action</div>
+			</div>
+
+			<div class='groupSaveContent' rel='10'>
+				<div class='header'>Superbowl 5/1-5/15</div>
+					<div><img src='/images/superbowl.jpg' class='img-thumbnail' style='width: 100%'></div>
+					<div><img src='/images/profile1.jpg' class='img-circle' style='width: 40px; float:left;'></div>
+				<div style='float: left;'>Phitha<br>xx minutes ago</div>
+				<div style='clear: both;'></div>
+				<div>Detail</div>
+				<div>Action</div>
+			</div>
+
+			<div class='groupSaveContent' rel='10'>
+				<div class='header'>Sonoma Camping 6/1-6/15</div>
+					<div><img src='/images/camping.jpg' class='img-thumbnail' style='width: 100%'></div>
+					<div><img src='/images/profile1.jpg' class='img-circle' style='width: 40px; float:left;'></div>
+				<div style='float: left;'>Phitha<br>xx minutes ago</div>
+				<div style='clear: both;'></div>
+				<div>Detail</div>
+				<div>Action</div>
+			</div>
+		</div>
+	</div>
+</div>
