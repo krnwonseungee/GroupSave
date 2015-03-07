7c06893a3a8f7403a5f6e2eeda3e642cb3ae6a90
Janet Yi <janetyi90@gmail.com>
2015-03-07 14:29:54 -0800

    Adds search bar

--- hack.invite.php
+++ hack.invite.php
@@ -3,6 +3,9 @@
     <div id='invite-header-box' class='dTab'>
       <div class='dTabHeader'>Invite List for Hawaii Trip</div>
   </div>
+<div class="search-bar">
+  <input type="text" name="friendName">
+</div>
 <div class="friend-list">
   <div class="friend-box">
     <i class="fa fa-plus-circle"></i> Friend 1
