e9e84a6a092bc5f7f0df85dd0e68959002f1aad9
Samuel Fahlberg <sam.fahlberg@gmail.com>
2015-03-07 19:06:52 -0800

    improve trips page

--- hack.trips.php
+++ hack.trips.php
@@ -1,36 +1,42 @@
-<div class='container-fluid row'>
-	<div class='col-xs-12'>
-		<div id='groupSave' class='dTab'>
+<div class="col-md-6">
+<a class='trip'
+	href='#'>
+	<img id='user' src='/images/johnny.jpg' class='img-circle' style='width: 40px; float:left;'>
+	<img 
+	  src='/images/hawaii.jpg' 
+	  class="img-thumbnail img-responsive index-img">
+	<span class="text-content index-img img">Hawaii Trip<br>($2000)</span>
+</a>
+</div>
 
-			<div class='groupSaveContent' rel='10'>
-				<div class='header'>Hawaii Trip 4/1-4/15</div>
-					<div><img src='/images/hawaii.jpg' class='img-thumbnail' style='width: 100%'></div>
-					<div><img src='/images/profile1.jpg' class='img-circle' style='width: 40px; float:left;'></div>
-				<div style='float: left;'>Phitha<br>xx minutes ago</div>
-				<div style='clear: both;'></div>
-				<div>Detail</div>
-				<div>Action</div>
-			</div>
+<div class="col-md-6">
+<a class='trip'
+	href='#'>
+	<img id='user' src='/images/vlad.jpg' class='img-circle' style='width: 40px; float:left;'>
+	<img 
+	  src='/images/camping.jpg' 
+	  class="img-thumbnail img-responsive index-img">
+	<span class="text-content index-img img">Camping<br>($500)</span>
+</a>
+</div>
 
-			<div class='groupSaveContent' rel='10'>
-				<div class='header'>Superbowl 5/1-5/15</div>
-					<div><img src='/images/superbowl.jpg' class='img-thumbnail' style='width: 100%'></div>
-					<div><img src='/images/profile1.jpg' class='img-circle' style='width: 40px; float:left;'></div>
-				<div style='float: left;'>Phitha<br>xx minutes ago</div>
-				<div style='clear: both;'></div>
-				<div>Detail</div>
-				<div>Action</div>
-			</div>
+<div class="col-md-6">
+<a class='trip'
+	href='#'>
+	<img id='user' src='/images/hillary.jpg' class='img-circle' style='width: 40px; float:left;'>
+	<img 
+	  src='/images/burning.jpg' 
+	  class="img-thumbnail img-responsive index-img">
+	<span class="text-content index-img img">Burning Man<br>($900)</span>
+</a>
+</div>
 
-			<div class='groupSaveContent' rel='10'>
-				<div class='header'>Sonoma Camping 6/1-6/15</div>
-					<div><img src='/images/camping.jpg' class='img-thumbnail' style='width: 100%'></div>
-					<div><img src='/images/profile1.jpg' class='img-circle' style='width: 40px; float:left;'></div>
-				<div style='float: left;'>Phitha<br>xx minutes ago</div>
-				<div style='clear: both;'></div>
-				<div>Detail</div>
-				<div>Action</div>
-			</div>
-		</div>
-	</div>
+<div class="col-md-6">
+<a class='trip'
+	href='#'>
+	<img 
+	  src='/images/burning.jpg' 
+	  class="img-thumbnail img-responsive index-img">
+	<span class="text-content index-img img">Start a new trip!</span>
+</a>
 </div>
